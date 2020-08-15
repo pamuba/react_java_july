023133df0fe6d5d3f3076fd344de8626068c9e3f
pamuba <pamuba@users.noreply.github.com>
2020-08-16 00:16:44 +0530

    66

--- reactApp/reactapp/src/App.js
+++ reactApp/reactapp/src/App.js
@@ -12,12 +12,16 @@ import UserGreeting from './components/UserGreeting';
 import NameList from './components/NameList';
 import Stylesheet from './components/Stylesheet';
 import Inline from './components/Inline';
+import './appStyles.css';
+import styles from './appStyles.module.css';
 
 
 class App extends Component{
   render(){
     return (
       <div className="App">
+      <h1 className='error'>Error</h1>
+      <h1 className={styles.success}>Success</h1>
        {/* <Greet /> */}
        {/* <Hello name="Bruce" heroName="Batman">
          <p>Data for childern props1</p>
